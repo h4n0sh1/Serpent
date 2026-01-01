8e6fc3f03615048334f3141133956b23aec3e36d
h4n0sh1 <h4n0sh1@gmail.com>
2026-01-01 17:36:37 +0100

    ai is so good :D

--- src/hw_test.go
+++ src/hw_test.go
@@ -8,14 +8,14 @@ import (
 
 func TestEncryptAndCopy(t *testing.T) {
 	// Copy contents from test to val folder
-	err := encrypt.CopyFolderContents("test", "val")
+	err := encrypt.CopyFolderContents("../test", "../val")
 	if err != nil {
 		t.Fatalf("Error copying folders: %v", err)
 	}
 
 	// Encrypt the contents of val folder
 	key := []byte("a-32-byte-key-for-AES-256-!!!!!!") // 32 bytes
-	err = encrypt.EncryptFolder("val", key)
+	err = encrypt.EncryptFolder("../val", key)
 	if err != nil {
 		t.Fatalf("Error encrypting val folder: %v", err)
 	}
